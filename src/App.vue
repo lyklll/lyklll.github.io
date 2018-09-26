56eee9f23027c60f0c57306c1129bbe5b3416936
Jam <lyklll@hotmail.com>
2018-09-27 01:23:23 +0800

    VUE3+VANT

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      
+        <van-tabbar v-model="active">
+            <van-tabbar-item icon="home">首页</van-tabbar-item>
+            <van-tabbar-item icon="play" dot>游乐指南</van-tabbar-item>
+            <van-tabbar-item icon="search" info="5">发现</van-tabbar-item>
+            <van-tabbar-item icon="contact" info="20">我的</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            active: 2
+        }
+
+    }
+
+}
+</script>
+<style>
+</style>
